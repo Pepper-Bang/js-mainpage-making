ddad34bbd744e6a255244a80fb4e8d5e8e5faa03
박동윤 <ehddbs7458@gmail.com>
2020-03-11 06:54:27 +0900

    finish toDoList

--- toDoList.js
+++ toDoList.js
@@ -1,12 +1,39 @@
 const toDoForm = document.querySelector(".js-form_toDoList"),
   toDoInput = toDoForm.querySelector("input"),
-  toDoList = document.querySelector("js-toDoList");
+  toDoList = document.querySelector(".js-toDoList");
 
 const TODO_LS = "toDos";
 
-function enterToDo(text){
-    const li=document.createElement("li");
-    const li_removeBtn=document.createElement()
+let toDosArray = [];
+
+function saveToDos() {
+  localStorage.setItem(TODO_LS, JSON.stringify(toDosArray));
+}
+
+function enterToDo(text) {
+  const li = document.createElement("li");
+  const li_delBtn = document.createElement("button");
+  const li_delIcon = document.createElement("i");
+  li_delIcon.classList.add("fas");
+  li_delIcon.classList.add("fa-trash-alt");
+  li_delIcon.classList.add("fa-2x");
+  li_delBtn.appendChild(li_delIcon);
+  const li_span = document.createElement("span");
+  li_span.innerText = text;
+  li_Id = toDosArray.length + 1;
+  li.appendChild(li_delBtn);
+  li.appendChild(li_span);
+  li.id = li_Id;
+  toDoList.appendChild(li);
+  // li_delBtn.addEventListener("click",delToDo) delToDo만들때까지 대기
+
+  const toDoObj = {
+    text: text,
+    id: li_Id
+  };
+
+  toDosArray.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit_toDo(event) {
@@ -16,12 +43,13 @@ function handleSubmit_toDo(event) {
   toDoInput.value = null;
 }
 
-
-
 function loadToDos() {
   const toDos = localStorage.getItem(TODO_LS);
   if (toDos !== null) {
-    callToDos(toDos);
+    const parsedToDos = JSON.parse(toDos);
+    parsedToDos.forEach(function(toDo) {
+      enterToDo(toDo.text);
+    });
   }
 }
 
