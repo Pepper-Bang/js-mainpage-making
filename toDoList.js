e4aacbcaa3d72d7b4a951dc1221b1736a5f6ca87
박동윤 <ehddbs7458@gmail.com>
2020-03-13 23:51:34 +0900

    to do list finished without arraying ids

--- toDoList.js
+++ toDoList.js
@@ -1,6 +1,6 @@
-const toDoForm = document.querySelector(".js-form_toDoList"),
-  toDoInput = toDoForm.querySelector("input"),
-  toDoList = document.querySelector(".js-toDoList");
+const ToDoForm = document.querySelector(".js-form_toDoList"),
+  ToDoInput = ToDoForm.querySelector("input"),
+  ToDoList = document.querySelector(".js-toDoList");
 
 const TODO_LS = "toDos";
 
@@ -10,44 +10,56 @@ function saveToDos() {
   localStorage.setItem(TODO_LS, JSON.stringify(toDosArray));
 }
 
+function delToDo(event) {
+  let targetLi = event.target.parentNode;
+
+  ToDoList.removeChild(targetLi);
+  let resetToDosArray=toDosArray.filter(function(toDo){
+    // console.log(toDo.id, targetLi.id);
+    return toDo.id !=targetLi.id;
+  })
+  toDosArray =resetToDosArray;
+  saveToDos();
+
+
 function enterToDo(text) {
   const li = document.createElement("li");
-  const li_delBtn = document.createElement("button");
-  const li_delIcon = document.createElement("i");
-  li_delIcon.classList.add("fas");
-  li_delIcon.classList.add("fa-trash-alt");
-  li_delIcon.classList.add("fa-2x");
-  li_delBtn.appendChild(li_delIcon);
-  const li_span = document.createElement("span");
-  li_span.innerText = text;
-  li_Id = toDosArray.length + 1;
-  li.appendChild(li_delBtn);
-  li.appendChild(li_span);
-  li.id = li_Id;
-  toDoList.appendChild(li);
-  // li_delBtn.addEventListener("click",delToDo) delToDo만들때까지 대기
+    li.classList.add("to-do-list");
+  const liDelBtn = document.createElement("button");
+    liDelBtn.innerText = "✖";
+  const liSpan = document.createElement("span");
+
+  li.appendChild(liDelBtn);
+  li.appendChild(liSpan);
+  liSpan.innerText = text;
+  ToDoList.appendChild(li);
+
+  let liId = toDosArray.length + 1;
+  li.id=liId;
 
   const toDoObj = {
     text: text,
-    id: li_Id
+    id: liId
   };
 
   toDosArray.push(toDoObj);
   saveToDos();
+
+  liDelBtn.addEventListener("click", delToDo);
 }
 
 function handleSubmit_toDo(event) {
   event.preventDefault();
-  const currentValue_toDo = toDoInput.value;
+  const currentValue_toDo = ToDoInput.value;
   enterToDo(currentValue_toDo);
-  toDoInput.value = null;
+  ToDoInput.value = null;
 }
 
 function loadToDos() {
   const toDos = localStorage.getItem(TODO_LS);
   if (toDos !== null) {
     const parsedToDos = JSON.parse(toDos);
-    parsedToDos.forEach(function(toDo) {
+    parsedToDos.forEach(function (toDo) {
       enterToDo(toDo.text);
     });
   }
@@ -55,7 +67,7 @@ function loadToDos() {
 
 function init() {
   loadToDos();
-  toDoForm.addEventListener("submit", handleSubmit_toDo);
+  ToDoForm.addEventListener("submit", handleSubmit_toDo);
 }
 
 init();
